a33e49698b5ed87482b834dacf6c48c28c2d648b
ericklarsen <ericklarsenn@gmail.com>
2021-01-16 20:09:54 +0700

    Set-up global variable for styling

---
+++ src/styles/_variables.js
@@ -0,0 +1,232 @@
+export const font = {
+  regular: "Proxima-Nova-Reg",
+  semiBold: "Proxima-Nova-SemiBold",
+  bold: "Proxima-Nova-Bold",
+};
+
+export const fontSize = {
+  hxl: "60px",
+  h1: "40px",
+  h2: "34px",
+  h3: "28px",
+  h4: "24px",
+  h5: "20px",
+  h6: "18px",
+  body1: "16px",
+  body2: "14px",
+  caption: "12px",
+  small: "10px",
+};
+
+export const lineHeight = {
+  hxl: "72px",
+  h1: "48px",
+  h2: "40px",
+  h3: "34px",
+  h4: "30px",
+  h5: "24px",
+  h6: "22px",
+  body1: "20px",
+  body2: "18px",
+  caption: "14px",
+  small: "12px",
+};
+
+export const letterSpace = {
+  hxl: "-0.56px",
+  h1: "-0.48px",
+  h2: "-0.36px",
+  h3: "-0.24px",
+  h4: "-0.24px",
+  h5: "-0.24px",
+  h6: "-0.24px",
+  body1: "-0.12px",
+  body2: "-0.08px",
+  caption: "0",
+  small: "0",
+};
+
+export const blackShades = {
+  black800: "#1f1f1f",
+  black700: "#333333",
+  black600: "#545454",
+  black500: "#757575",
+  black400: "#afafaf",
+  black300: "#e2e2e2",
+  black200: "#eeeeee",
+};
+
+export const redShades = {
+  red800: "#5a0a00",
+  red700: "#870f00",
+  red600: "#ab1300",
+  red500: "#e11900",
+  red400: "#e85c4a",
+  red300: "#f1998e",
+  red200: "#fed7d2",
+  red100: "#ffefed",
+};
+
+export const greenShades = {
+  green800: "#10462d",
+  green700: "#156a41",
+  green600: "#09864a",
+  green500: "#05a357",
+  green400: "#06c167",
+  green300: "#66d19e",
+  green200: "#addec9",
+  green100: "#e6f2ed",
+};
+
+export const orangeShades = {
+  orange800: "#672a16",
+  orange700: "#9a3f21",
+  orange600: "#c14f29",
+  orange500: "#ff6937",
+  orange400: "#fa9269",
+  orange300: "#fabda5",
+  orange200: "#ffe1d6",
+  orange100: "#fef3ef",
+};
+
+export const blueShades = {
+  blue800: "#102c60",
+  blue700: "#174291",
+  blue600: "#1e54b7",
+  blue500: "#276ef1",
+  blue400: "#5b91f5",
+  blue300: "#a0bff8",
+  blue200: "#d4e2fc",
+  blue100: "#eff3fe",
+};
+
+export const yellowShades = {
+  yellow800: "#674d1b",
+  yellow700: "#997328",
+  yellow600: "#bc8b2c",
+  yellow500: "#ffc043",
+  yellow400: "#ffcf70",
+  yellow300: "#ffe3ac",
+  yellow200: "#fff2d9",
+  yellow100: "#fffaf0",
+};
+
+export const darkBlueShades = {
+  darkBlue800: "#050c4d",
+  darkBlue700: "#081270",
+  darkBlue600: "#0a1899",
+  darkBlue500: "#0e1fc1",
+  darkBlue400: "#535fcf",
+  darkBlue300: "#949ce3",
+  darkBlue200: "#d2d7f0",
+  darkBlue100: "#ebedfa",
+};
+
+export const purpleShades = {
+  purple800: "#2e224c",
+  purple700: "#453473",
+  purple600: "#574191",
+  purple500: "#7356bf",
+  purple400: "#c1b5e3",
+  purple300: "#949ce3",
+  purple200: "#e3ddf2",
+  purple100: "#f4f1fa",
+};
+
+export const brownShades = {
+  brown800: "#3d281e",
+  brown700: "#5c3c2e",
+  brown600: "#744c3a",
+  brown500: "#99644c",
+  brown400: "#d2bbb0",
+  brown300: "#ebe0db",
+  brown200: "#ebe0db",
+  brown100: "#f6f0ea",
+};
+
+export const grayShades = {
+  gray800: "#3d281e",
+  gray700: "#5c3c2e",
+  gray600: "#744c3a",
+  gray500: "#99644c",
+  gray400: "#d2bbb0",
+  gray300: "#ebe0db",
+  gray200: "#ebe0db",
+  gray100: "#f6f0ea",
+};
+
+export const breakPoint = {
+  mobileXS: 280,
+  mobileS: 320,
+  mobileM: 375,
+  mobileL: 425,
+  mobileXL: 576,
+  tablet: 768,
+  tabletXL: 900,
+  laptop: 1024,
+  laptopL: 1440,
+  desktop: 2560,
+};
+
+export const type = [
+  {
+    name: "normal",
+  },
+  {
+    name: "fighting",
+  },
+  {
+    name: "flying",
+  },
+  {
+    name: "poison",
+  },
+  {
+    name: "ground",
+  },
+  {
+    name: "rock",
+  },
+  {
+    name: "bug",
+  },
+  {
+    name: "ghost",
+  },
+  {
+    name: "steel",
+  },
+  {
+    name: "fire",
+  },
+  {
+    name: "water",
+  },
+  {
+    name: "grass",
+  },
+  {
+    name: "electric",
+  },
+  {
+    name: "psychic",
+  },
+  {
+    name: "ice",
+  },
+  {
+    name: "dragon",
+  },
+  {
+    name: "dark",
+  },
+  {
+    name: "fairy",
+  },
+  {
+    name: "unknown",
+  },
+  {
+    name: "shadow",
+  },
+];
