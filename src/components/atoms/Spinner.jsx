8cacd4e3172c2197cc57b298c6e7f4a367db8c8a
ericklarsen <ericklarsenn@gmail.com>
2021-01-16 20:23:22 +0700

    Loading spinner component

---
+++ src/components/atoms/Spinner.jsx
@@ -0,0 +1,26 @@
+import styled, { keyframes } from "styled-components";
+
+import React from "react";
+import Img from "./Img";
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const Image = styled(Img)`
+  width: 60px;
+  object-fit: contain;
+  opacity: 0.5;
+  animation: ${spin} 1s linear infinite;
+`;
+
+function Spinner(props) {
+  return <Image src="/static/img/ic_pokeball.png" {...props} />;
+}
+
+export default Spinner;
