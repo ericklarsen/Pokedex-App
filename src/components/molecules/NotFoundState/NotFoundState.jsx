fac6b216bb176a30ed9efcc88949e8c3c21cc2c7
ericklarsen <ericklarsenn@gmail.com>
2021-01-17 17:30:10 +0700

    Add reusable NotFoundState component

---
+++ src/components/molecules/NotFoundState/NotFoundState.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Img from "../../atoms/Img";
+import Typo from "../../atoms/Typography";
+import { blackShades } from "../../../styles/_variables";
+
+const { black400 } = blackShades;
+
+const NotFoundState = () => {
+  return (
+    <>
+      <Img
+        src="/static/img/ic_pokemon_logo_outline.png"
+        maxWidth="200px"
+        style={{ opacity: 0.5 }}
+      />
+      <Typo color={black400} margin="8px 0 0 0">
+        Not Found
+      </Typo>
+    </>
+  );
+};
+
+NotFoundState.propTypes = {};
+
+export default NotFoundState;
