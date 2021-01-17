78f368d3f56b7ed14237f014524b58a0f6f26bbf
ericklarsen <ericklarsenn@gmail.com>
2021-01-17 12:15:47 +0700

    Add customize head component

--- src/components/atoms/CommonHead.jsx
+++ src/components/atoms/CommonHead.jsx
@@ -8,9 +8,8 @@ const CommonHead = (props) => {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="theme-color" content="#00b900" />
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" />
+      <link rel="icon" href="/static/img/ic_loading.png" />
+      <link rel="apple-touch-icon" href="/static/img/ic_loading.png" />
       {props.children}
     </Head>
   );
